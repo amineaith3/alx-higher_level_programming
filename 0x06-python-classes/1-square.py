2a86077e592926f4cdd5d05a4ff32dfe07ff9708
amineaith3 <amineaithamma2004@gmail.com>
2023-10-24 14:30:09 -0700

    More commits

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-    """Define a class Square."""
-    class Square:
-        """ Square"""
-        def __init__(self, size):
-            """ Initialisation of the square
-            Args:
-            size(int): the size of the square.
-            """
-            self.__size = size
+"""Define a class Square."""
+class Square:
+    """ Square"""
+    def __init__(self, size):
+        """ Initialisation of the square
+        Args:
+        size(int): the size of the square.
+        """
+        self.__size = size
