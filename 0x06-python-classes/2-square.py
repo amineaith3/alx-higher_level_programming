47621dea94c25fc7104c90120c79df9e5f4dfd9b
amineaith3 <amineaithamma2004@gmail.com>
2023-10-24 14:39:17 -0700

    More commits

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,12 +5,12 @@
 class Square:
     """ Square"""
     def __init__(self, size=0):
-        """Initialisation of the square 
+        """Initialisation of the square
         Args:
-            size(int): the size of the square.
+        size(int): the size of the square.
         """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
-            raise ValueError("size must be >=0")
+            raise ValueError("size must be >= 0")
         self.__size = size
