c24dd43fa61dd995d51a9d97650503deed96146b
amineaith3 <amineaithamma2004@gmail.com>
2023-10-24 14:20:31 -0700

    More commits

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+    """Define a class Square."""
+    class Square:
+        """ Square"""
+        def __init__(self, size=0):
+            """ Initialisation of the square
+            Args:
+            size(int): the size of the square.
+            """
+            if not isinstance(size, int):
+                raise TypeError("size must be an integer")
+            elif size<0:
+                raise ValueError("size must be >=0")
+            self.__size = size
+
