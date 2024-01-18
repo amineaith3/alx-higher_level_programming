5e7c7e39cdd4ea99025f41f99ecff0d14b4d62fb
sakuun_aah <91127128+amineaith3@users.noreply.github.com>
2024-01-18 11:20:01 +0100

    Update 1-multi_languages.js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const arr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 arr.forEach(function (el) {
   console.log(el);
 });
