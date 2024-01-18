67cac5f93efe6d79ee5440e0a0ac0d52e2638e25
sakuun_aah <91127128+amineaith3@users.noreply.github.com>
2024-01-18 11:16:41 +0100

    Update 9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -4,6 +4,6 @@ function add (a, b) {
   const result = num1 + num2;
   return result;
 }
-var num1 = parseInt(Number(arg[2]));
-var num2 = parseInt(Number(arg[3]));
+let num1 = parseInt(Number(arg[2]));
+let num2 = parseInt(Number(arg[3]));
 console.log(add(num1, num2));
